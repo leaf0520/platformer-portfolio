f4ce42764882498488921ad5cc03c376b4b10b71
leaf3262 <leaf3262@gmail.com>
2026-08-06 11:58:21 +0800

    Add ground detection and jump mechanics

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -4,12 +4,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
 
     private PlayerInputActions inputActions;
     private Vector2 moveInput;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
     private Rigidbody2D rb;
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -22,11 +27,13 @@ public class PlayerController : MonoBehaviour
     private void OnEnable()
     {
         inputActions.Player.Enable();
+        inputActions.Player.Jump.performed += OnJumpPerformed;
     }
 
     private void OnDisable()
     {
         inputActions.Player.Disable();
+        inputActions.Player.Jump.performed -= OnJumpPerformed;
     }
 
     private void Update()
@@ -49,5 +56,15 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
