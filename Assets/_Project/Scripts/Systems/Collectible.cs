eaaac94c08e903bc8ea246a8c75f62343c53803d
leaf3262 <leaf3262@gmail.com>
2026-08-08 10:36:35 +0800

    Add particle burst effect on coin pickup

--- Assets/_Project/Scripts/Systems/Collectible.cs
+++ Assets/_Project/Scripts/Systems/Collectible.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     [SerializeField] private int scoreValue = 10;
+    [SerializeField] private GameObject pickupEffectPrefab;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             ScoreManager.Instance.AddScore(scoreValue);
+
+            if (pickupEffectPrefab != null)
+            {
+                Instantiate(pickupEffectPrefab, transform.position, Quaternion.identity);
+            }
+
             Destroy(gameObject);
         }
     }
